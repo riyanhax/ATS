4bfa6086a4bba6dd1a073eaaf4bfc5f8a7e2f884
MarcoSchaefer <marcoatschaefer@gmail.com>
2018-07-21 18:19:58 -0300

    switch fixed. Panel now shows data in real time

--- js/popup.js
+++ js/popup.js
@@ -1,43 +1,47 @@
 function updateState() {
-  chrome.tabs.executeScript({
-      code: `getConfigs();`
-    },
-    function(result) {
-      config = result[0];
-      document.getElementById("state_active_up").innerText = config.active.up;
-      document.getElementById("state_active_down").innerText = config.active.down;
-      document.getElementById("peak_variation").value = config.peak_variation;
-      document.getElementById("history_length").value = config.history_length;
-    }
-  );
+    chrome.tabs.executeScript({
+            code: `getState();`
+        },
+        function(result) {
+            config = result[0].config;
+            state = result[0];
+            document.getElementById("state_active_up").innerText = config.active.up;
+            document.getElementById("state_active_down").innerText = config.active.down;
+            document.getElementById("peak_variation").value = config.peak_variation;
+            document.getElementById("history_length").value = config.history_length;
+            document.getElementById("mean").innerText = state.mean;
+            document.getElementById("peak").innerText = state.peak;
+        }
+    );
+    setTimeout(updateState, 1000);
 }
 
 function toggleActiveUp() {
-  chrome.tabs.executeScript({
-    code: `config.active.up = !config.active.up;`
-  });
-  updateState();
+    chrome.tabs.executeScript({
+        code: `config.active.up = !config.active.up;`
+    });
+    updateState();
 }
 
 function toggleActiveDown() {
-  chrome.tabs.executeScript({
-    code: `config.active.down = !config.active.down;`
-  });
-  updateState();
+    chrome.tabs.executeScript({
+        code: `config.active.down = !config.active.down;`
+    });
+    updateState();
 }
 
 function setPeakVariation() {
-  chrome.tabs.executeScript({
-    code: `config.peak_variation = `+document.getElementById('peak_variation').value+`;`
-  });
-  updateState();
+    chrome.tabs.executeScript({
+        code: `config.peak_variation = ` + document.getElementById('peak_variation').value + `;`
+    });
+    updateState();
 }
 
 function setHistoryLength() {
-  chrome.tabs.executeScript({
-    code: `config.history_length = `+document.getElementById('history_length').value+`;`
-  });
-  updateState();
+    chrome.tabs.executeScript({
+        code: `config.history_length = ` + document.getElementById('history_length').value + `;`
+    });
+    updateState();
 }
 
 document.getElementById('toggle_active_up').addEventListener('click', toggleActiveUp);
